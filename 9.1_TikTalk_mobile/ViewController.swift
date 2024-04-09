2ff225b7c4fe4df48ccc045b419ebd9e80a111fd
SazonovAlexander <aleksandr.sazonow03@gmail.com>
2024-04-09 22:07:41 +0300

    Initial Commit

---
+++ 9.1_TikTalk_mobile/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  9.1_TikTalk_mobile
+//
+//  Created by Александр Сазонов on 09.04.2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
