138fed56779ae003c14f675f474351f76d748e8c
SazonovAlexander <aleksandr.sazonow03@gmail.com>
2024-04-10 23:53:29 +0300

    create band screen

---
+++ 9.1_TikTalk_mobile/Band/BandViewController.swift
@@ -0,0 +1,88 @@
+import UIKit
+
+enum BandType: String {
+    case all = "Общая"
+    case subscriptions = "Подписки"
+}
+
+final class BandViewController: UIViewController {
+    
+    private lazy var bandTypeSegment: UISegmentedControl = {
+        let segment = UISegmentedControl(items: [BandType.all.rawValue, BandType.subscriptions.rawValue])
+        segment.selectedSegmentIndex = 0
+        segment.selectedSegmentTintColor = .white
+        segment.setTitleTextAttributes(
+            [NSAttributedString.Key.foregroundColor: UIColor.white],
+            for:.normal
+        )
+        segment.setTitleTextAttributes(
+            [NSAttributedString.Key.foregroundColor: UIColor.black],
+            for:.selected
+        )
+        return segment
+    }()
+    
+    private lazy var podcastView: PodcastView = {
+        let podcastView = PodcastView()
+        return podcastView
+    }()
+    
+    private lazy var autoLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 24, weight: .medium)
+        label.numberOfLines = 1
+        label.text = "Автопереход"
+        return label
+    }()
+    
+    private lazy var autoSwitch: UISwitch = {
+        let autoSwitch = UISwitch()
+        autoSwitch.onTintColor = .blue
+        return autoSwitch
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+}
+
+private extension BandViewController {
+    
+    func setup() {
+        setupAppearance()
+        addSubviews()
+        activateConstraints()
+    }
+    
+    func setupAppearance() {
+        view.backgroundColor = .background
+    }
+    
+    func addSubviews() {
+        [bandTypeSegment,
+         podcastView,
+         autoLabel,
+         autoSwitch].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
+    
+    func activateConstraints() {
+        NSLayoutConstraint.activate([
+            bandTypeSegment.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            bandTypeSegment.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            bandTypeSegment.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            podcastView.topAnchor.constraint(equalTo: bandTypeSegment.bottomAnchor, constant: 20),
+            podcastView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor),
+            podcastView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            autoLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            autoLabel.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor),
+            autoSwitch.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            autoSwitch.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
