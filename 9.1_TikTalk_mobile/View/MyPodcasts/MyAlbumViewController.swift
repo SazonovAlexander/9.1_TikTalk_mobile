0891fc8ddf1fa97efac85dac2aa51a3ecb54cfc7
SazonovAlexander <aleksandr.sazonow03@gmail.com>
2024-04-21 16:32:36 +0300

    TT-28: Create my album screen

---
+++ 9.1_TikTalk_mobile/View/MyPodcasts/MyAlbumViewController.swift
@@ -0,0 +1,135 @@
+import UIKit
+
+
+final class MyAlbumViewController: UIViewController {
+    
+    private let symbolButtonStackConfiguration = UIImage.SymbolConfiguration(pointSize: 44)
+    
+    private lazy var nameLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 24, weight: .medium)
+        label.text = "Название альбома" //название альбома
+        label.textAlignment = .center
+        label.numberOfLines = 1
+        return label
+    }()
+    
+    private lazy var descriptionButton: UIButton = {
+        let button = UIButton()
+        button.setImage(UIImage(systemName: "text.justify", withConfiguration: symbolButtonStackConfiguration), for: .normal)
+        button.imageView?.tintColor = .white
+        return button
+    }()
+    
+    private lazy var editButton: BaseButtonView = {
+        let button = BaseButtonView()
+        button.config(text: "Редактировать", backgroundColor: UIColor(named: "ProfileButtonBackground") ?? .blue)
+        return button
+    }()
+    
+    private lazy var deleteButton: BaseButtonView = {
+        let button = BaseButtonView()
+        button.config(text: "Удалить", backgroundColor: UIColor(named: "ButtonRed") ?? .red)
+        return button
+    }()
+    
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.textColor = .white
+        label.font = .systemFont(ofSize: 24, weight: .medium)
+        label.text = "Подкасты"
+        return label
+    }()
+    
+    private lazy var tableView: UITableView = {
+        let tableView = UITableView()
+        tableView.backgroundColor = .none
+        tableView.separatorColor = .lightGray
+        tableView.register(PodcastViewCell.self, forCellReuseIdentifier: PodcastViewCell.reuseIdentifier)
+        tableView.dataSource = self
+        tableView.delegate = self
+        return tableView
+    }()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+}
+
+private extension MyAlbumViewController {
+    
+    func setup() {
+        setupAppearance()
+        addSubviews()
+        activateConstraints()
+    }
+    
+    func setupAppearance() {
+        view.backgroundColor = UIColor(named: "Background")
+    }
+    
+    func addSubviews() {
+        [nameLabel,
+        descriptionButton,
+        editButton,
+        deleteButton,
+        titleLabel,
+        tableView].forEach{
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
+    
+    func activateConstraints() {
+        NSLayoutConstraint.activate([
+            descriptionButton.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor),
+            nameLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            nameLabel.trailingAnchor.constraint(lessThanOrEqualTo: descriptionButton.leadingAnchor),
+            descriptionButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            nameLabel.centerYAnchor.constraint(equalTo: descriptionButton.centerYAnchor),
+            editButton.topAnchor.constraint(equalTo: nameLabel.bottomAnchor, constant: 20),
+            editButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            editButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            deleteButton.topAnchor.constraint(equalTo: editButton.bottomAnchor, constant: 20),
+            deleteButton.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            deleteButton.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            deleteButton.heightAnchor.constraint(equalToConstant: 50),
+            editButton.heightAnchor.constraint(equalToConstant: 50),
+            titleLabel.topAnchor.constraint(equalTo: deleteButton.bottomAnchor, constant: 20),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 16),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
+            tableView.topAnchor.constraint(equalTo: titleLabel.bottomAnchor, constant: 10),
+            tableView.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            tableView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            tableView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
+        ])
+    }
+}
+
+extension MyAlbumViewController: UITableViewDelegate {
+    
+}
+
+extension MyAlbumViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        10 //заменить на датасорс
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: PodcastViewCell.reuseIdentifier, for: indexPath)
+                       
+        guard let podcastCell = cell as? PodcastViewCell else {
+           return UITableViewCell()
+        }
+        
+        podcastCell.config(name: "Подкаст \(indexPath.row)", image: UIImage(named: "Logo") ?? UIImage())//заменить на датасорс
+
+        return podcastCell
+    }
+    
+    func tableView(_ tableView: UITableView, heightForRowAt indexPath: IndexPath) -> CGFloat {
+        60
+    }
+}
