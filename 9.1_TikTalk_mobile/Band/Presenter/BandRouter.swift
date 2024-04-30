4496a2f349f5fbb1111c1f0d25c85a28a7780c57
SazonovAlexander <aleksandr.sazonow03@gmail.com>
2024-04-30 20:43:34 +0300

    TT-70: Update flow

--- 9.1_TikTalk_mobile/Band/Presenter/BandRouter.swift
+++ 9.1_TikTalk_mobile/Band/Presenter/BandRouter.swift
@@ -7,7 +7,6 @@ final class BandRouter {
         let presenter = DescriptionPresenter(description: description)
         let descriptionViewController = DescriptionViewController(presenter: presenter)
         presenter.viewController = descriptionViewController
-        descriptionViewController.modalPresentationStyle = .formSheet
         viewController.present(descriptionViewController, animated: true)
     }
     
@@ -15,20 +14,26 @@ final class BandRouter {
         let presenter = AuthorPresenter(author: author)
         let authorViewController = AuthorProfileViewController(presenter: presenter)
         presenter.viewController = authorViewController
-        viewController.navigationController?.pushViewController(authorViewController, animated: true)
+        let navController = UINavigationController(rootViewController: authorViewController)
+        navController.navigationBar.backItem?.title = "Назад"
+        viewController.present(navController, animated: true)
     }
     
     func showAlbumFrom(_ viewController: UIViewController, album: AlbumModel) {
         let presenter = AlbumPresenter(album: album)
         let albumViewController = AlbumViewController(presenter: presenter)
         presenter.viewController = albumViewController
-        viewController.navigationController?.pushViewController(albumViewController, animated: true)
+        let navController = UINavigationController(rootViewController: albumViewController)
+        navController.navigationBar.backItem?.title = "Назад"
+        viewController.present(navController, animated: true)
     }
     
     func showReportFrom(_ viewController: UIViewController, podcast: PodcastModel) {
         let presenter = ReportPresenter(podcast: podcast)
         let reportViewController = ReportViewController(presenter: presenter)
         presenter.viewController = reportViewController
-        viewController.navigationController?.pushViewController(reportViewController, animated: true)
+        let navController = UINavigationController(rootViewController: reportViewController)
+        navController.navigationBar.backItem?.title = "Назад"
+        viewController.present(navController, animated: true)
     }
 }
