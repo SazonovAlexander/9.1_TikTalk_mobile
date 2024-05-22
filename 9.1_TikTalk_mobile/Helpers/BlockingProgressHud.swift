f0ed1650df9d5acf3da4034078e4c1c27e58e448
SazonovAlexander <aleksandr.sazonow03@gmail.com>
2024-05-22 22:24:19 +0300

    TT-50: Add blocking progress hud

---
+++ 9.1_TikTalk_mobile/Helpers/BlockingProgressHud.swift
@@ -0,0 +1,20 @@
+import UIKit
+import ProgressHUD
+
+
+final class BlockingProgressHUD {
+    
+    private static var window: UIWindow? {
+            return UIApplication.shared.windows.first
+        }
+    
+    static func show() {
+        window?.isUserInteractionEnabled = false
+        ProgressHUD.animate(interaction: false)
+    }
+    
+    static func dismiss() {
+        window?.isUserInteractionEnabled = true
+        ProgressHUD.dismiss()
+    }
+}
