29b36b42e61589618b1c89a570fb28b39bb8e50c
SazonovAlexander <aleksandr.sazonow03@gmail.com>
2024-05-27 20:55:04 +0300

    TT-113: Create onboarding flow

---
+++ 9.1_TikTalk_mobile/Onboarding/OnboardingViewController.swift
@@ -0,0 +1,70 @@
+import UIKit
+
+
+final class OnboardingViewController: UIViewController {
+
+    let text: (String, String)
+
+    private lazy var titleLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 24, weight: .medium)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.text = text.0
+        return label
+    }()
+
+    private lazy var textLabel: UILabel = {
+        let label = UILabel()
+        label.font = .systemFont(ofSize: 24, weight: .regular)
+        label.textColor = .white
+        label.numberOfLines = 0
+        label.textAlignment = .center
+        label.text = text.1
+        return label
+    }()
+
+    init(text: (String, String)) {
+        self.text = text
+        super.init(nibName: nil, bundle: nil)
+    }
+
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        setup()
+    }
+}
+
+private extension OnboardingViewController {
+
+    func setup() {
+        view.backgroundColor = UIColor(named: "Background")
+        addSubviews()
+        activateConstraints()
+    }
+
+    func addSubviews() {
+        [titleLabel,
+         textLabel].forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            view.addSubview($0)
+        }
+    }
+
+    func activateConstraints() {
+        let superView: UIView = view.superview ?? view
+        NSLayoutConstraint.activate([
+            titleLabel.topAnchor.constraint(equalTo: view.safeAreaLayoutGuide.topAnchor, constant: 40),
+            titleLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            titleLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20),
+            textLabel.centerYAnchor.constraint(equalTo: view.safeAreaLayoutGuide.centerYAnchor),
+            textLabel.leadingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.leadingAnchor, constant: 20),
+            textLabel.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor, constant: -20)
+        ])
+    }
+}
