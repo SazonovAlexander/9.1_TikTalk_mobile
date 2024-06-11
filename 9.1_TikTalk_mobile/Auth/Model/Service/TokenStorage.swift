4a68b6f117b1f971e05b66af7697b48308552540
SazonovAlexander <aleksandr.sazonow03@gmail.com>
2024-06-11 23:50:17 +0300

    TT-121: Create token storage

---
+++ 9.1_TikTalk_mobile/Auth/Model/Service/TokenStorage.swift
@@ -0,0 +1,39 @@
+import Foundation
+import SwiftKeychainWrapper
+
+
+final class OAuth2TokenStorage {
+    
+    static let shared = OAuth2TokenStorage()
+    
+    private static let accessTokenKey = "access_token"
+    private static let refreshTokenKey = "refresh_token"
+    
+    private init() {}
+     
+    var accessToken: String {
+        get {
+            return KeychainWrapper.standard.string(forKey: OAuth2TokenStorage.accessTokenKey) ?? ""
+        }
+        set (newToken){
+            let isSuccess = KeychainWrapper.standard.set(newToken, forKey: OAuth2TokenStorage.accessTokenKey)
+            guard isSuccess else {
+                print("Token writing error")
+                return
+            }
+        }
+    }
+    
+    var refreshToken: String {
+        get {
+            return KeychainWrapper.standard.string(forKey: OAuth2TokenStorage.refreshTokenKey) ?? ""
+        }
+        set (newToken){
+            let isSuccess = KeychainWrapper.standard.set(newToken, forKey: OAuth2TokenStorage.refreshTokenKey)
+            guard isSuccess else {
+                print("Token writing error")
+                return
+            }
+        }
+    }
+}
